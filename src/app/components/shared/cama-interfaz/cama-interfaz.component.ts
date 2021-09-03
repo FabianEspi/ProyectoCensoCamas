7d199a9f7baacfa91b86ad6ea387c24b9437208d
fabian.espitia.mayasoft <fabian.espitia@mayasoft.com.co>
2021-09-02 19:41:35 -0500

    iniciando interfazCamas v1

--- src/app/components/shared/cama-interfaz/cama-interfaz.component.ts
+++ src/app/components/shared/cama-interfaz/cama-interfaz.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+
 import { CamasService } from '../../../services/camas/camas.service';
 
 
@@ -9,16 +11,28 @@ import { CamasService } from '../../../services/camas/camas.service';
 })
 export class CamaInterfazComponent implements OnInit {
 
+  cargando:boolean= false;
+  nodata:boolean = false;
+  tipoCama;
+
+  
 
   //GRUPOS
   grupitos=[];
   grupos={};
   //SUBGRUPOS
   subgrupo={} ;
-  subgrupitos=[];
+  subgrupitos = [];
+  
   //TIPOSDECAMA
   tipo={};
   tipito=[];
+
+  //CAMAS
+  camas = {};
+  camitas = [];
+  estados = []
+
   
   constructor(private camasService:CamasService) {
 
@@ -38,6 +52,40 @@ export class CamaInterfazComponent implements OnInit {
 
 
     this.camasService.findCamaGrupoSubgrupoTipo(grupo,subgrupo,tipo).subscribe(resp => {
+
+      
+      this.tipoCama = tipo;
+      this.camas = resp;
+      let valorPrueba = Object.keys(this.camas).length;
+      console.log(this.camas);
+
+      if(this.camitas.length === 0){
+
+        this.cargando = true;
+        setTimeout(() => {
+
+
+          for (let index = 0; valorPrueba; index++) {
+            this.camitas.push( this.camas[index]);
+            this.estados.push(this.camas[index].estadoCama);
+            let source = {URL:'URL ESTADO 2'}
+            Object.assign(this.camitas[index],source)
+            this.cargando=false;
+          }
+
+          console.log(this.camitas);
+          
+        }, 1000);
+
+      
+
+
+        
+        
+        
+
+      }
+      
 
 
 
@@ -64,7 +112,7 @@ export class CamaInterfazComponent implements OnInit {
 
       this.tipo = resp;
       let valor = Object.keys(this.tipo).length;
-      if(this.tipito.length <= 0){
+      if(this.tipito.length === 0){
 
         for (let index = 0; valor; index++) {
           this.tipito.push( this.tipo[index].nombre);
@@ -87,14 +135,13 @@ export class CamaInterfazComponent implements OnInit {
         this.grupos=resp;
       let valor = Object.keys(this.grupos).length;
       
-      if(this.grupitos.length <= 0){
+      if(this.grupitos.length === 0){
 
       
 
           for (let index = 0; valor; index++) {
             this.grupitos.push( this.grupos[index].nombre);
           }
-          
 
     }
 
@@ -110,20 +157,51 @@ export class CamaInterfazComponent implements OnInit {
 
   this.camasService.findGrupoWithNombre(grupo).subscribe(resp=>{
 
+    
     let oidSubgrupo =resp["id"]
 
     this.camasService.findSubgrupoWithGrupoOID(oidSubgrupo).subscribe(resp =>{
 
       this.subgrupo = resp;
       let valor = Object.keys(this.subgrupo).length
+      console.log(valor);
+      
+      
+
+      
   
-      if(this.subgrupitos.length <= 0){
+
+      
   
+      if(this.subgrupitos.length === 0 && valor!= 0){
+
         for (let index = 0; index < valor; index++) {
           this.subgrupitos.push( this.subgrupo[index]);
-    
         }
-  }
+
+        
+     } 
+
+    
+
+     if( valor === 0){
+
+      if(this.subgrupitos.length === 0 ){
+      
+      this.subgrupitos.push("No hay subgrupos");
+
+       
+
+
+      }
+
+      
+      
+
+     }
+
+
+    
   },(err)=>{
     console.log("Error en el servicio de encontrar  los subgrupos");
   });
